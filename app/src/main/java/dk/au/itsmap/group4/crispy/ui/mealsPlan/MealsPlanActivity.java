a3fbf349db9009b65c3dd1ed2acdb4956b85c2c3
pbnather <p.belczak@me.com>
2018-12-03 16:37:28 +0100

    Add basic authentication to MealPlan activity

--- app/src/main/java/dk/au/itsmap/group4/crispy/ui/mealsPlan/MealsPlanActivity.java
+++ app/src/main/java/dk/au/itsmap/group4/crispy/ui/mealsPlan/MealsPlanActivity.java
@@ -1,25 +1,27 @@
 package dk.au.itsmap.group4.crispy.ui.mealsPlan;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
 import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.Navigation;
 import dk.au.itsmap.group4.crispy.R;
+import dk.au.itsmap.group4.crispy.ui.CrispyAuthenticatedActivity;
 
-public class MealsPlanActivity extends AppCompatActivity {
+public class MealsPlanActivity extends CrispyAuthenticatedActivity {
 
     Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.meals_plan_activity);
 
-
-        mToolbar = (Toolbar) findViewById(R.id.mainToolbar);
+        mToolbar = findViewById(R.id.mainToolbar);
         mToolbar.setTitle(R.string.crispy_planner);
         setSupportActionBar(mToolbar);
 
@@ -51,25 +53,5 @@ public class MealsPlanActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
-
-    //From: https://developer.android.com/training/appbar/actions
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.btnGroceryList:
-               //TODO: navigate to grocery list
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-
-        }
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        return Navigation.findNavController(this, R.id.navigation_fragment).navigateUp();
-    }
-
-
 
 }
