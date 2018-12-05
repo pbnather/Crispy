049c1b7be585839bdd2ac068c5b7ea006e43d06e
pbnather <p.belczak@me.com>
2018-12-05 18:15:57 +0100

    Make AuthActivity use AuthViewModel for authentication

--- app/src/main/java/dk/au/itsmap/group4/crispy/ui/AuthActivity.java
+++ app/src/main/java/dk/au/itsmap/group4/crispy/ui/AuthActivity.java
@@ -2,22 +2,17 @@ package dk.au.itsmap.group4.crispy.ui;
 
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.bumptech.glide.request.Request;
 import com.bumptech.glide.request.target.SimpleTarget;
-import com.bumptech.glide.request.target.SizeReadyCallback;
 import com.bumptech.glide.request.transition.Transition;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,9 +20,9 @@ import java.util.List;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModelProviders;
 import dk.au.itsmap.group4.crispy.R;
-import dk.au.itsmap.group4.crispy.database.FSRepository;
-import dk.au.itsmap.group4.crispy.model.IUserGroup;
 import dk.au.itsmap.group4.crispy.service.GlideApp;
 
 public abstract class AuthActivity extends AppCompatActivity implements INavigationActivity {
@@ -35,100 +30,22 @@ public abstract class AuthActivity extends AppCompatActivity implements INavigat
     private static final String TAG = "AuthActivity";
     private static final int RC_SIGN_IN = 451;
 
-    private final FirebaseAuth mAuth = FirebaseAuth.getInstance();
-    private AuthVIewModel mModel;
-
-    protected Menu mMenu;
-    protected FirebaseUser mCurrentUser;    // V
-    protected IUserGroup mUserGroup;        // M
+    private Menu mMenu;
+    private AuthViewModel mAuth;
+    private FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Authenticate user on start of activity
-        authenticateUser();
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        mMenu = menu;
-        updateMenu();
-        return true;
-    }
+        mAuth = ViewModelProviders.of(this).get(AuthViewModel.class);
 
-    /* Adapted from https://stackoverflow.com/a/37116931 */
-    private void updateMenu() {
-        GlideApp.with(this)
-                .load(mCurrentUser != null ? mCurrentUser.getPhotoUrl() : null)
-                //.placeholder(ContextCompat.getDrawable(this, R.drawable.default_profile_picture))
-                .into(new SimpleTarget<Drawable>() {
-                    @Override
-                    public void onStart() {
-
-                    }
-
-                    @Override
-                    public void onStop() {
-
-                    }
-
-                    @Override
-                    public void onDestroy() {
-
-                    }
-
-                    @Override
-                    public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
-                        mMenu.findItem(R.id.btnAccount).setIcon(resource);
-                    }
-
-                    @Override
-                    public void onLoadCleared(@Nullable Drawable placeholder) {
-
-                    }
-
-                    @Override
-                    public void removeCallback(@NonNull SizeReadyCallback cb) {
-
-                    }
-
-                    @Override
-                    public void setRequest(@Nullable Request request) {
-
-                    }
-
-                    @Nullable
-                    @Override
-                    public Request getRequest() {
-                        return null;
-                    }
-
-                    @Override
-                    public void onLoadStarted(@Nullable Drawable placeholder) {
-
-                    }
-
-                    @Override
-                    public void onLoadFailed(@Nullable Drawable errorDrawable) {
-                        //mMenu.findItem(R.id.btnAccount).setIcon(errorDrawable);
-                    }
-                });
-    }
-
-    @Override
-    /* Adapted from https://developer.android.com/training/appbar/actions */
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.btnGroceryList:
-                return true;
-            case R.id.btnAccount:
-                getNavController().navigate(R.id.accountFragment);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-
-        }
+        LiveData<FirebaseUser> firebaseUser = mAuth.getCurrentUser();
+        firebaseUser.observeForever(user -> {
+            if (user == null) signIn();
+            updateMenu(user);
+            mUser = user;
+        });
     }
 
     @Override
@@ -140,37 +57,59 @@ public abstract class AuthActivity extends AppCompatActivity implements INavigat
 
             // Successfully signed in
             if (resultCode == RESULT_OK) {
-                authenticateUser();
-                updateMenu();
+                updateMenu(mUser);
                 if (response != null && response.isNewUser()) {
-                    registerUser(mCurrentUser.getUid(), mCurrentUser.getDisplayName());
+                    mAuth.registerUser();
                 }
             } else {
                 if (response == null) {
                     signIn();
-                    return;
                 } else if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
                     // TODO: Notify user about no internet connection
-                    return;
+                } else {
+                    // TODO: Notify user about unknown error
+                    Log.e(TAG, "Sign-in error: ", response.getError());
                 }
-
-                // TODO: Notify user about unknown error
-                Log.e(TAG, "Sign-in error: ", response.getError());
             }
         }
     }
 
-    private void authenticateUser() {
-        // Get user, if null -> sing in
-        mCurrentUser = mAuth.getCurrentUser();  // V
-        if(mCurrentUser == null) {              // M
-            signIn();
-        // if userGroup is not loaded -> load it
-        } else if(mUserGroup == null)           // V
-            FSRepository                        // M
-                    .getInstance()
-                    .getUserGroup(mCurrentUser.getUid())
-                    .observe(this, group -> mUserGroup = group);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        mMenu = menu;
+        updateMenu(mUser);
+        return true;
+    }
+
+    @Override
+    /* Adapted from https://developer.android.com/training/appbar/actions */
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.btnGroceryList:
+                signOut();
+                return true;
+            case R.id.btnAccount:
+                getNavController().navigate(R.id.accountFragment);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    /* Adapted from https://stackoverflow.com/a/37116931 */
+    private void updateMenu(FirebaseUser user) {
+        if(mMenu == null) return;
+        GlideApp.with(this)
+                .asDrawable()
+                .load(user != null ? user.getPhotoUrl() : null)
+                .placeholder(R.drawable.default_profile_picture)
+                .into(new SimpleTarget<Drawable>() {
+                    @Override
+                    public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+                        mMenu.findItem(R.id.btnAccount).setIcon(resource);
+                    }});
     }
 
     private void signIn() {
@@ -186,33 +125,9 @@ public abstract class AuthActivity extends AppCompatActivity implements INavigat
                         .build(), RC_SIGN_IN);
     }
 
-    // must be callable from fragments
     protected void signOut() {
         AuthUI.getInstance()
                 .signOut(this).addOnSuccessListener(task -> {
-            authenticateUser();
         });
-    }
-
-    private void registerUser(String userId, String username) { // V
-        FSRepository                                            // M
-                .getInstance()
-                .createUserWithGroup(userId, username);
-    }
-
-    private void updateUserProfile(String username, String photoUrl) {
-        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                .setDisplayName(username)
-                .setPhotoUri(Uri.parse(photoUrl))
-                .build();
-
-        if(mCurrentUser != null) {                      // V
-            mCurrentUser.updateProfile(profileUpdates)  // M
-                    .addOnCompleteListener(task -> {
-                        if (task.isSuccessful()) {
-                            updateMenu();
-                        }
-                    });
-        }
     }
 }
---
+++ app/src/main/java/dk/au/itsmap/group4/crispy/ui/AuthViewModel.java
@@ -0,0 +1,55 @@
+package dk.au.itsmap.group4.crispy.ui;
+
+import android.app.Application;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import dk.au.itsmap.group4.crispy.database.FSRepository;
+
+public class AuthViewModel extends AndroidViewModel {
+
+    private final FirebaseAuth mAuth = FirebaseAuth.getInstance();;
+    private final FirebaseAuthLiveData mUser = new FirebaseAuthLiveData();
+    private final FSRepository mRepository = FSRepository.getInstance();
+
+    public AuthViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public FirebaseAuthLiveData getCurrentUser() {
+        return mUser;
+    }
+
+    public void registerUser() {
+        FirebaseUser user = mUser.getValue();
+        if(user != null) {
+            String userId = mUser.getValue().getUid();
+            String userName = mUser.getValue().getDisplayName();
+            mRepository.createUserWithGroup(userId, userName == null ? "" : userName);
+        }
+    }
+
+    /* Adapted from https://stackoverflow.com/q/50035915 */
+    private class FirebaseAuthLiveData extends LiveData<FirebaseUser> {
+        private FirebaseAuth.AuthStateListener authStateListener =
+                firebaseAuth -> {
+                    FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                    setValue(firebaseUser);   };
+
+        @Override
+        protected void onActive() {
+            super.onActive();
+            mAuth.addAuthStateListener(authStateListener);
+        }
+
+        @Override
+        protected void onInactive() {
+            super.onInactive();
+            mAuth.removeAuthStateListener(authStateListener);
+        }
+    }
+}
