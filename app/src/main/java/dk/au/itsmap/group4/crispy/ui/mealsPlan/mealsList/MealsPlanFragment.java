8550a50fd93271463f16e665c0e0faaca9316e35
pbnather <p.belczak@me.com>
2018-12-07 12:50:00 +0100

    Fix toolbar in MealsPlanFragment

--- app/src/main/java/dk/au/itsmap/group4/crispy/ui/mealsPlan/mealsList/MealsPlanFragment.java
+++ app/src/main/java/dk/au/itsmap/group4/crispy/ui/mealsPlan/mealsList/MealsPlanFragment.java
@@ -52,7 +52,7 @@ public class MealsPlanFragment extends Fragment implements MealsPlanRecyclerView
         btnRecipies = mView.findViewById(R.id.btnAllRecipies);
         btnAddMeal = mView.findViewById(R.id.btnAddMeal);
 
-        ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        ActionBar actionBar = mActivity.getSupportActionBar();
         actionBar.setTitle(R.string.crispy_planner);
         actionBar.setDisplayHomeAsUpEnabled(false);
         actionBar.setDisplayShowHomeEnabled(false);
